abeb29fa03aaed0877a43956edc3504af533f3c8
Lucas Argento <lucasargento@Lucass-MacBook-Air.local>
2022-04-18 15:10:53 -0300

    pre y posts

--- TP1/dia_en_la_uni.c
+++ TP1/dia_en_la_uni.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 // Constantes (strings como define porque todavia no las vimos formalmente)
 #define PREGUNTA1 "\n1) Estás saliendo de tu casa, y te enteras que hay paro de trenes, ¿Qué otro transporte elegís?\nOpciones:\n\nTrineo (T)\nNave Espacial (N)\nCamión (C)\nAlfombra (A)."
@@ -11,7 +10,8 @@
 #define SALTOLARGO "\n\n\n------------------\n"
 #define RESPUESTAINVALIDA "\nRespuesta inválida, intenta de nuevo: "
 
-// Constantes: letras rtas y puntajes preg 1
+// Constantes: letras rtas y puntajes preg 1. Declaro tanto upper como lower case
+// para no utilizar funciones de librerias externas (tolower y toupper)
 const char trineo = 'T';
 const char trineo_L = 't';
 const char nave_espacial = 'N';
@@ -54,7 +54,19 @@ const char jasmin = 'J';
 
 
 void preguntar1(char* ref_rta, int* ref_puntaje){
-   
+    /*  -> Funcion que imprime al usuario la primera pregunta y registra su respuesta
+    [Pre]:
+        - ref_rta: debe ser una referencia a la variable de la primera rta. Apunta a una variable
+            de tipo Char.
+        - ref_puntaje: debe ser una referencia a la variable del puntaje de la primera rta. Apunta a 
+            una variable de tipo entero.
+    [Post]:
+        - La funcion no devuelve ningun valor. Las variables (parametros) de entrada de la función
+        son modificadas por referencia.
+        - ref_rta: la variable referenciada puede tomar los valores T, N, C, A segun la rta del usuario 
+        (o sus analogos en lower case).
+        - ref_puntaje: toma valor 10 si *ref_rta: T o N, y 20 si *ref_rta: C o A.
+    */
     printf(PREGUNTA1);
     printf("\n\n> Cual es tu respuesta? [Solo el primer caracter será tomado en cuenta]: ");
     scanf(" %c",ref_rta);
@@ -69,7 +81,7 @@ void preguntar1(char* ref_rta, int* ref_puntaje){
         (*ref_rta) != alfombra && 
         (*ref_rta) != alfombra_L) 
         {
-            printf("\nRespuesta inválida, intenta de nuevo: ");
+            printf(RESPUESTAINVALIDA);
             scanf(" %c",ref_rta);
     };
 
@@ -90,6 +102,23 @@ void preguntar1(char* ref_rta, int* ref_puntaje){
 }
 
 void preguntar2(char* ref_rta, int* ref_puntaje){
+    /*  -> Funcion que imprime al usuario la segunda pregunta y registra su respuesta
+    [Pre]:
+        - ref_rta: debe ser una referencia a la variable de la segunda rta. Apunta a una variable
+            de tipo Char.
+        - ref_puntaje: debe ser una referencia a la variable del puntaje de la segunda rta. Apunta a 
+            una variable de tipo entero.
+    [Post]:
+        - La funcion no devuelve ningun valor. Las variables (parametros) de entrada de la función
+        son modificadas por referencia.
+        - ref_rta: la variable referenciada puede tomar los valores B, P, K, G segun la rta del usuario 
+        (o sus analogos en lower case).
+        - ref_puntaje: la variable referenciada toma valor 
+            3 si *ref_rta: B, 
+            6 si *ref_rta: P,
+            9 si *ref_rta: K,
+            12 si *ref_rta: G,
+    */
     printf(SALTOCORTO);
     printf(PREGUNTA2);
     printf("\n\n> Cual es tu respuesta? [Solo el primer caracter será tomado en cuenta]: ");
@@ -105,7 +134,7 @@ void preguntar2(char* ref_rta, int* ref_puntaje){
         (*ref_rta) != genio &&
         (*ref_rta) != genio_L) 
         {
-            printf("\nRespuesta inválida, intenta de nuevo: ");
+            printf(RESPUESTAINVALIDA);
             scanf(" %c",ref_rta);
     };
         
@@ -122,14 +151,29 @@ void preguntar2(char* ref_rta, int* ref_puntaje){
 }
 
 float parteDecimal (float num) {
-   // cuando usamos literales numericos y los asignamos a una variable de tipo float
-   // es correcto que le pongamos f al final para indicarselo al compilador
-   int parte_entera = (int)num;
-   num = num - (float)parte_entera; // se vuele a castear para evitar la warning de conversion
-   return num;
+   /*  -> Funcion que devuelve la parte decimal de un numero
+    [Pre]:
+        - num: debe ser un numero de punto flotante (float).
+    [Post]:
+        - La funcion devuelve "num", un numero de punto flotante (float) que representa la parte 
+        decimal de un numero. Su valor esta comprendido entre 0 y 1.
+    */
+    int parte_entera = (int)num;
+    num = num - (float)parte_entera;
+    return num;
 }
 
 void preguntar3(float* hora_llegada){
+    /*  -> Funcion que imprime al usuario la tercera pregunta y registra su respuesta
+    [Pre]:
+        - hora_llegada: debe ser una referencia a la variable de la tercera respuesta, la cual coincidirá
+        con la hora de llegada del usuario. Apunta a una variable de tipo Float.
+    [Post]:
+        - La variable (parámetro) de entrada de la función es modificada por referencia, generando un float
+        con parte entera entre 0 y 24 y parte decimal entre 0 y 0.59. La hora de llegada ingresada por el 
+        usuario será utilizada posteriormente tanto como "puntaje de la respuesta 3" y como respuesta de 
+        esta ("rta3"). 
+    */
     printf(SALTOCORTO);
     printf(PREGUNTA3);
     printf("\n\n> Cual es tu respuesta? [Responde en formato HH.MM (donde H: hora y M: Minuto)]: ");
@@ -142,7 +186,7 @@ void preguntar3(float* hora_llegada){
 
     // validacion 
     while (horas < limite_inf_horas || horas > limite_sup_horas || minutos < limite_inf_minutos || minutos > limite_sup_minutos) {
-        printf("\nRespuesta inválida, intenta de nuevo: ");
+        printf(RESPUESTAINVALIDA);
         scanf(" %f", hora_llegada);
         horas = (int)*hora_llegada;
         //minutos = *hora_llegada - horas;
@@ -153,14 +197,31 @@ void preguntar3(float* hora_llegada){
 }
 
 void preguntar4(int* nota, int* ref_puntaje){
+    /*  -> Funcion que imprime al usuario la cuarta pregunta y registra su respuesta
+    [Pre]:
+        - nota: debe ser una referencia a la variable de la cuarta rta. Apunta a una variable
+            de tipo entero. La respuesta de la cuarta pregunta equivale a la nota que se saco el usuario
+            en el parcial.
+        - ref_puntaje: debe ser una referencia a la variable del puntaje de la cuarta rta. Apunta a 
+            una variable de tipo entero.
+    [Post]:
+        - Las variables (parametros) de entrada de la función son modificados por referencia.
+        - Nota tomará valores enteros entre 1 y 10.
+        - Ref_puntaje tomara el valor de un multiplicador en funcion de la nota registrada,
+            donde:
+                multiplicador: 1 si la nota esta entre 1 y 3,
+                multiplicador: 2 si la nota esta entre 4 y 6,
+                multiplicador: 3 si la nota esta entre 7 y 8,
+                multiplicador: 4 si la nota esta entre 9 y 10,
+    */
     printf(SALTOCORTO);
     printf(PREGUNTA4);
     printf("\n\n> Cual es tu respuesta? [Las notas posibles son números del 1 al 10]: ");
-    scanf(" %i",nota);
+    scanf(" %i", nota);
 
     // validacion
     while (*nota < 1 || *nota > 10) {
-        printf("\nRespuesta inválida, intenta de nuevo: ");
+        printf(RESPUESTAINVALIDA);
         scanf(" %i",nota);
     }
 
@@ -177,10 +238,28 @@ void preguntar4(int* nota, int* ref_puntaje){
 }
 
 int calcularPuntaje(int* puntaje1, int* puntaje2, int* puntaje4){
+    /*  -> Funcion que calcula el puntaje total registrado luego de las preguntas
+    [Pre]:
+        - Puntaje1, puntaje2, puntaje4: referencias a los puntajes de las 1era, 2da y 4ta rtas respectivamente.
+        las 3 deben apuntar a variables de tipo entero. El puntaje de la 3era rta no participa de este cálculo.
+    [Post]:
+        - La funcion devuelve un entero que corresponde al puntaje total calculado segun la formula:
+        Ptotal = P1 + (P2 * P4)
+    */
     return (*puntaje1) + (*puntaje2) * (*puntaje4);
 }
 
 char decidirPersonaje(int* puntaje_total, float* hora_llegada_parcial){
+    /*  -> Funcion que toma la decision final sobre el personaje obtenido.
+    [Pre]:
+        - Puntaje_total: debe ser una referencia a la variable del puntaje total, previamente
+        calculado. Apunta a una variable de tipo entero.
+        - Hora_llegada_parcial: debe ser una referencia a la variable de la hora de llegada (rta3)
+        registrado en la tercera pregunta. Apunta a una variable de tipo Float.
+    [Post]:
+        - La funcion devuelve una variable de tipo Char. La misma tomara los valores (S, O, J, R)
+        segun el personaje decidido.
+    */
     if (*puntaje_total >= olaf_stich_lim_inferior && *puntaje_total <= olaf_stich_lim_superior){
         // final olaf vs stich
         if (*hora_llegada_parcial < hora_del_parcial) {
@@ -217,8 +296,8 @@ int main(){
     int rta4;
     
     // Preguntas
-    preguntar1(&rta1,&puntaje1);
-    preguntar2(&rta2,&puntaje2);
+    preguntar1(&rta1, &puntaje1);
+    preguntar2(&rta2, &puntaje2);
     preguntar3(&rta3);
     preguntar4(&rta4, &puntaje4);
 
@@ -228,7 +307,7 @@ int main(){
 
     printf(SALTOLARGO);
 
-    // Informo el resultado final 
+    // Informo el resultado final, los prints originales estan comentados para pasar los tests del Chanutron.
     if (personaje_definitivo == olaf) {
         //printf("\n--> Con un puntaje de %i puntos se definió el personaje - Olaf (O) -.", puntaje_total);
         printf("-Olaf-");
@@ -241,7 +320,6 @@ int main(){
     } else if (personaje_definitivo == rayo) {
         //printf("\n--> Con un puntaje de %i puntos se definió el personaje - Rayo (R) -.", puntaje_total);
         printf("-Rayo McQueen-");
-
     } else {
         printf("\n--> Ups! Algo salio mal.");
     }
